36afaa1785dfb1fd30e246dc4d6850d05a7df192
Deiviidsito <david.alvarez.barraza@gmail.com>
2024-05-23 13:13:08 -0400

    first

---
+++ lib/presentation/screens/chat/chat_screen.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:yes_no_app/presentation/widgets/chat/my_message_bubble.dart';
+
+class ChatScreen extends StatelessWidget {
+  const ChatScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: const Padding(
+            padding: EdgeInsets.all(4.0),
+            child: CircleAvatar(
+              backgroundImage: NetworkImage(
+                  'https://a.wattpad.com/useravatar/Danixutv2020.256.98653.jpg'),
+            )),
+        title: const Text('Deiviid'),
+        centerTitle: false,
+      ),
+      body: _ChatView(),
+    );
+  }
+}
+
+class _ChatView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10),
+        child: Column(
+          children: [
+            Expanded(child: ListView.builder(itemBuilder: (context, index) {
+              return const MyMessageBubble();
+            })),
+            Text('Hola mundo!')
+          ],
+        ),
+      ),
+    );
+  }
+}
